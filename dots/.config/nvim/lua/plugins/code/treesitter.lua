170eb41082bcfa775fda67e1e76c4a7a93cf3893
rick <thatmfrick@proton.me>
2026-03-28 18:06:02 +0100

    Updating dots

--- dots/.config/nvim/lua/plugins/code/treesitter.lua
+++ dots/.config/nvim/lua/plugins/code/treesitter.lua
@@ -1,60 +1,38 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
+	lazy = false,
 	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
 	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
+		-- We use a pcall (protected call) to prevent Neovim from
+		-- crashing if the compiled library is briefly missing.
+		local status, configs = pcall(require, "nvim-treesitter.configs")
+		if not status then
+			return
+		end
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-			-- ensure these language parsers are installed
+		configs.setup({
+			-- ensures these are always installed on every machine
 			ensure_installed = {
-				-- "json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				-- "sh",
 				"lua",
 				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
 				"vimdoc",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"bash",
 				"c",
-				"sql",
-				"kotlin",
-				-- "latex",
 			},
-			incremental_selection = {
+
+			-- automatically install missing parsers when you open a new file type
+			auto_install = true,
+
+			highlight = {
+				enable = true, -- this is the most important part!
+				additional_vim_regex_highlighting = false,
+			},
+
+			indent = {
 				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
 			},
 		})
 	end,
